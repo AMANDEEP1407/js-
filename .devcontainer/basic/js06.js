2b29e9b484fd3741e5a947badde70e4898188e73
AMANDEEP1407 <162784911+AMANDEEP1407@users.noreply.github.com>
2024-06-04 08:33:37 +0000

    datatype

---
+++ .devcontainer/basic/js06.js
@@ -0,0 +1,17 @@
+let score = "33"
+//console.log(typeof score);
+//let valueInNumber=Number(score)
+//console.log(valueInNumber);
+//console.log(typeof valueInNumber)
+
+
+let login=""
+//let booleanislogin=Boolean(login)
+//console.log(booleanislogin)
+//console.log(typeof booleanislogin)
+
+
+let sone=1
+let Sones =String(sone);
+console.log(sones)
+console.log(typeof sones)
