9af0e2fc38a304e1d2015bec7662da2419bfafa1
AMANDEEP1407 <162784911+AMANDEEP1407@users.noreply.github.com>
2024-08-17 08:59:34 +0000

    de stur. object

---
+++ .devcontainer/basic/object3.js
@@ -0,0 +1,11 @@
+const course={
+    coursename:"JS",
+    courseprice:"999",
+    courseteacher:"hitish"
+
+}
+//course.courseteacher
+const {courseteacher:thr}=course//destucture object
+//console.log(courseteacher);
+console.log(thr);
+
