5770e114756803c24f6ccd2e678f3b9e6abb0adb
AMANDEEP1407 <162784911+AMANDEEP1407@users.noreply.github.com>
2024-08-17 10:26:28 +0000

    functions

---
+++ .devcontainer/basic/functions.js
@@ -0,0 +1,32 @@
+function example()
+{
+    console.log("1")
+    console.log("2")
+    console.log("3")
+
+}
+//example()
+function addtwonum(num1,num2)
+{
+console.log(num1+num2);
+}
+//addtwonums(2,4)
+
+
+function addtwonums(num1,num2)
+{
+     let result=num1+num2
+console.log("aman");
+return result
+}
+const result=addtwonums(2,4)
+console.log("result:",result);
+
+
+function logged(username) {
+    return username//'${username} logged in' this not working
+
+    
+}
+
+console.log(logged("anuj"));
