3f7505f8fcfe08787322d44504772e437160b298
AMANDEEP1407 <162784911+AMANDEEP1407@users.noreply.github.com>
2024-08-17 11:37:41 +0000

    function2

---
+++ .devcontainer/basic/function2.js
@@ -0,0 +1,32 @@
+function calcartprice(num){
+    return num
+}
+//console.log(calcartprice(200))
+function calcartprice1(val1,val2,...num){
+    return num
+}
+//console.log(calcartprice1(200,300,100,2000)
+
+const user=
+{
+    username:"raman",
+    prices:199
+}
+function handleobject(anyobject)
+{
+console.log(username,price)
+}
+//can pass direct object
+//handle object user
+//
+const mynewarr=[200,300,400 ,500]
+
+function returnsecondvalue(getarr)
+
+{
+    return getarr[1]
+}
+//console.log(returnsecondvalue(mynewarr));
+console.log(returnsecondvalue([200,300,400,500]));
+
+//watch video 
