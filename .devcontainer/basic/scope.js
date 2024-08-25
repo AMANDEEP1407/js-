ef4fff38fc09f0ce20d05f8a3dd7a57f05ebddd9
AMANDEEP1407 <162784911+AMANDEEP1407@users.noreply.github.com>
2024-08-25 12:01:42 +0000

    scope

---
+++ .devcontainer/basic/scope.js
@@ -0,0 +1,45 @@
+function fun1()
+{
+     const Username="aman"
+
+    function fun2(){
+const  website="mywebsite"
+console.log(Username)
+    }
+   // console.log(website)
+    fun2()
+}
+fun1()
+    
+
+if(true)
+{
+    const username="aman"
+    if(username==="aman")
+        {
+            const website=" youtube"
+            console.log(username+website)
+
+        }
+      //  console.log(website)
+}
+//console.log(username)
+
+
+function fun3(num)
+    {
+ return num+1
+
+    }
+   console.log(fun3(5))
+
+   ////
+
+  // ReferenceError: Cannot access 'merafun' before initialization
+   merafun(5)
+   
+   const merafun= function fun4(num1) ///function store in varible
+{
+      return num+2
+
+}
